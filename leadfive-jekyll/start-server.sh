a4a5e7b81bb3119d0b18d050849db0836b986609
Greenroom51 <greenroom51@gmail.com>
2025-11-11 09:22:46 +0900

    Remove obsolete documentation and scripts related to HP infrastructure and LeadFive website, including architecture summaries, deployment guides, and various configuration files. This cleanup aims to streamline the project and eliminate unused resources.

--- leadfive-jekyll/start-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/leadfive/Desktop/system/022_HP/leadfive-jekyll
-echo "Starting Jekyll server..."
-eval "$(rbenv init -)"
-bundle exec jekyll serve --config _config.yml,_config_dev.yml --port 4000
